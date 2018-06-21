e2fad1b7ee48d053d491df865568bb4c96c5f9cc
Giancarlos Salas <giansalex@gmail.com>
2018-06-21 16:59:42 -0500

    del directory + writing log

--- index.php
+++ index.php
@@ -4,14 +4,34 @@ use Symfony\Component\Yaml\Yaml;
 
 require 'vendor/autoload.php';
 
+$eof = php_sapi_name() === 'cli' ? PHP_EOL : '<br>';
 function getNameClass($class) {
     $path = explode('\\', $class);
 
     return array_pop($path);
 }
 
+function delDir($path){
+    if(is_dir($path) == TRUE){
+        $rootFolder = scandir($path);
+        if(sizeof($rootFolder) > 2){
+            foreach($rootFolder as $folder){
+                if($folder != "." && $folder != ".."){
+                    delDir($path."/".$folder);
+                }
+            }
+            rmdir($path);
+        }
+    }else{
+        if(file_exists($path) == TRUE){
+            unlink($path);
+        }
+    }
+}
+
 $jms = new \Giansalex\Serializer\JmsGenerator();
 
+echo 'Convert classes'.$eof;
 $result = $jms->fromClasses([
     \Greenter\Model\Sale\Invoice::class,
     \Greenter\Model\Sale\Note::class,
@@ -25,13 +45,16 @@ $result = $jms->fromClasses([
 
 $pathDir = __DIR__.'/serializer';
 if (is_dir($pathDir)) {
-    rmdir($pathDir);
+    delDir($pathDir);
 }
 
 mkdir($pathDir);
 
+echo 'Writing yaml files'.$eof;
 foreach ($result as $class => $props) {
     $yaml = Yaml::dump([$class => $props], 4);
     $name = getNameClass($class);
     file_put_contents($pathDir.'/model.'.$name.'.yaml', $yaml);
 }
+
+echo 'Completed!'.$eof;
